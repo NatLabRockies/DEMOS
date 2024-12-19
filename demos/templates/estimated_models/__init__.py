fb587f0707ebd131e433c328e105c02419d02750
Guangchen Zhao <150304725+guangchenzhao@users.noreply.github.com>
2024-12-19 14:05:25 -0700

    update utils

--- demos/templates/estimated_models/__init__.py
+++ demos/templates/estimated_models/__init__.py
@@ -1,3 +1,4 @@
 from .multinomial_logit import MultinomialLogitStep
 from .binary_logit import BinaryLogitStep
+from .large_multinomial_logit import LargeMultinomialLogitStep
 from .shared import TemplateStep
