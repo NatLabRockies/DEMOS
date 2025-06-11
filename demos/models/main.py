bcf6581ec2000fb9b7f5126ef6fc56d73c69c4a3
Yamil <yaessus@ncsu.edu>
2025-06-11 11:31:21 -0400

    Small fix

--- demos/models/main.py
+++ demos/models/main.py
@@ -18,13 +18,13 @@ all_local = orca.get_injectable("all_local")
 if orca.get_injectable("running_calibration_routine") == False:
     if orca.get_injectable("local_simulation") == True:
         demo_models = [
-            # "update_age",
-            # "laborforce_model",
-            # "households_reorg",
-            # "kids_moving_model",
-            # "fatality_model",
-            # "birth_model",
-            # "education_model",
+            "update_age",
+            "laborforce_model",
+            "households_reorg",
+            "kids_moving_model",
+            "fatality_model",
+            "birth_model",
+            "education_model",
             "household_rebalancing",
             "update_income",
             # "export_demo_stats",
