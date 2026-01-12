2348d92db2e65532617f66f8053ebce411cff094
Yamil <yaessus@ncsu.edu>
2026-01-12 17:08:50 -0500

    run black

--- demos/models/kids_moving.py
+++ demos/models/kids_moving.py
@@ -138,7 +138,14 @@ def run_and_calibrate_model(persons):
     demos_config: DEMOSConfig = get_config()
     module_config: KidsMovingModuleConfig = demos_config.kids_moving_module_config
 
-    child_relate = [2, 3, 4, 7, 9, 14] # This is more `dependent` because `child` is determined by age
+    child_relate = [
+        2,
+        3,
+        4,
+        7,
+        9,
+        14,
+    ]  # This is more `dependent` because `child` is determined by age
     target_share = module_config.calibration_target_share
 
     # Get model data
@@ -149,23 +156,27 @@ def run_and_calibrate_model(persons):
     kids_moving = model.predict(model_data).astype(int)
 
     # NOTE: This could be much easier if we set the age at 18 because we could use model_filters
-    adult_filter = (persons.age >= 18)
+    adult_filter = persons.age >= 18
     age_moved = persons.age.loc[kids_moving[kids_moving == 1].index]
-    adult_stay = (adult_filter & persons.relate.isin(child_relate)).sum() - (age_moved >=18).sum()
+    adult_stay = (adult_filter & persons.relate.isin(child_relate)).sum() - (
+        age_moved >= 18
+    ).sum()
     observed_share = adult_stay / adult_filter.sum()
-    error = (observed_share - target_share)
+    error = observed_share - target_share
 
     print("Calibrating Kids moving model")
     calibrate_iteration = 0
-    while abs(error) > module_config.calibration_tolerance: 
+    while abs(error) > module_config.calibration_tolerance:
         print(f"{calibrate_iteration} iteration error: {error}")
-        model.fitted_parameters[0] += np.log(observed_share/target_share)
+        model.fitted_parameters[0] += np.log(observed_share / target_share)
 
         kids_moving = model.predict(model_data).astype(int)
         age_moved = persons.age.loc[kids_moving[kids_moving == 1].index]
-        adult_stay = (adult_filter & persons.relate.isin(child_relate)).sum() - (age_moved >=18).sum()
+        adult_stay = (adult_filter & persons.relate.isin(child_relate)).sum() - (
+            age_moved >= 18
+        ).sum()
         observed_share = adult_stay / adult_filter.sum()
-        error = (observed_share - target_share)
+        error = observed_share - target_share
 
         calibrate_iteration += 1
     print(f"{calibrate_iteration} iteration error: {error}")
